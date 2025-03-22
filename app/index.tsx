19379602b27b62e28ba33e1c850eb9b3f547ee5b
Anish <anisharma030@gmail.com>
2025-03-23 00:04:57 +0530

    Added basic color theme for the app

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-gray-500">Hello World!</Text>
+    <View className="flex-1 items-center justify-center bg-secondary">
+      <Text className="text-5xl text-light">Hello World!</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D1B2A",
+        secondary: "#1B263B",
+        light: "#E0E1DD",
+        dark: "#778DA9",
+        accent: "#415A77",
+      },
+    },
   },
   plugins: [],
 };
